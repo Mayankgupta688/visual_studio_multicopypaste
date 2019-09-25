c4cd834608627c598f61fb6e90150e525ba46507
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-09-25 16:23:31 +0530

    Updated Code...

--- extension.js
+++ extension.js
@@ -10,104 +10,68 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	var copiedValueString = new Array(10);
-	copiedValueString[0] = "0";
-	copiedValueString[1] = "1";
-	copiedValueString[2] = "2";
-	copiedValueString[3] = "3";
-	copiedValueString[4] = "4";
-	copiedValueString[5] = "5";
-	copiedValueString[6] = "6";
-	copiedValueString[7] = "7";
-	copiedValueString[8] = "8";
-	copiedValueString[9] = "9";
-
-
-	var functionCallCount = 0;
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "multiplecopypaste" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	var copiedValueString = new Array(10).fill('');
 
 	var disposable = null;
 	var commandName = "";
 
 	for(let i = 0; i< 10; i++) {
-
 		commandName = 'extension.multiplePaste' + i
 		disposable = vscode.commands.registerCommand(commandName, () => { 
 			pasteActivation(i);
 		});
-
 		context.subscriptions.push(disposable);
 	}
 
 	for(let i = 0; i< 10; i++) {
-
 		commandName = 'extension.multipleCopy' + i
 		disposable = vscode.commands.registerCommand(commandName, () => { 
 			copyActivation(i);
 		});
-
 		context.subscriptions.push(disposable);
 	}
 	
 
 	function pasteActivation(inputValue) {
-		
-		functionCallCount = inputValue;
-
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Call Count: ' + functionCallCount );
-
-		var editor = vscode.window.activeTextEditor;
-
-		if(!editor) {
-			vscode.window.showInformationMessage('No File is open in Editor...');
-			return;
-		}
-
-		editor.edit((editBuilder) => {
-
-			// editor.selection gives the current location of the cursor...
-			editBuilder.delete(editor.selection)
-		}).then(() => {
+		if(copiedValueString[inputValue]) {
+			var editor = vscode.window.activeTextEditor;
+			if(!editor) {
+				vscode.window.showInformationMessage('No File is open in Editor...');
+				return;
+			}
 			editor.edit((editBuilder) => {
-				editBuilder.insert(editor.selection.start, copiedValueString[inputValue]);
+				editBuilder.delete(editor.selection)
+			}).then(() => {
+				editor.edit((editBuilder) => {
+					vscode.window.showInformationMessage("Value at index " + inputValue + " pasted..." );
+					editBuilder.insert(editor.selection.start, copiedValueString[inputValue]);
+				})
 			})
-		})
+		} else {
+			vscode.window.showInformationMessage('No value saved at Position: ' + inputValue );
+		}
 	}
 
 
 	function copyActivation(inputValue) {
-		
-		functionCallCount = inputValue;
-
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Call Count: ' + functionCallCount );
-
-
 		vscode.window.activeTextEditor.selections.toString()
 		var editor = vscode.window.activeTextEditor;
-
 		var selectedValue = vscode.window.activeTextEditor.document.getText(editor.selection);
-
 		if(!editor) {
 			vscode.window.showInformationMessage('No File is open in Editor...');
 			return;
 		}
+		if(selectedValue) {
+			copiedValueString[inputValue] = selectedValue;
 
-		copiedValueString[inputValue] = selectedValue;
+			vscode.window.showInformationMessage('Copied value at Index: ' + inputValue );
+
+		} else {
+			vscode.window.showInformationMessage('No Text Selected...');
+		}
 	}
 }
+
 exports.activate = activate;
 
 function deactivate() {}
